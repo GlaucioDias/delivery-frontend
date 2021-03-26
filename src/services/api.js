2a4ca288b738bd933140830480ac1cac4277be30
GlaucioDias <fleevrl@gmail.com>
2021-03-25 23:42:17 -0300

    change api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://delivery-glaucio.herokuapp.com'
+    baseURL: 'https://mighty-ravine-45401.herokuapp.com'
 })
 
 export default api;
